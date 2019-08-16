5d498dc14189d109c0be0c0bbd05c18fc1ed1ecf
Try <khovct@gmail.com>
2019-08-15 20:38:52 -0700

    fixed the icons, got the app done, added the style text

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import EmployeeApp from './src/EmployeeApp';
@@ -50,19 +49,13 @@ const reducer = (state = initState, action) => {
 
 const store = createStore(reducer);
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+export default class App extends React.Component {
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  render() {
+    return (
+      <Provider store={store}>
+        <EmployeeApp/>
+      </Provider>
+    )
+  }
+};
--- src/EmployeeApp.js
+++ src/EmployeeApp.js
@@ -1,23 +1,108 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
+import { Card, Icon } from 'native-base';
+import { connect } from 'react-redux';
 
 
 
-export default class EmployeeApp extends React.Component {
+class EmployeeApp extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
-            </View>
-        );
-    }
+            <FlatList 
+              data={Object.values(this.props.data)}
+              renderItem={ ({item}) => (
+                <Card style={styles.container}>
+                  <View style={styles.idContainer}>
+                    <Text style={styles.idText}>{item.empID}</Text>
+                  </View>
+                  <View style={styles.nameAndSalaryContainer}>
+                    <Text style={styles.nameText}>Name: {item.empName}</Text>
+                    <Text style={styles.salaryText}>Salary: {item.empSalary.toFixed(2)}</Text>
+                  </View>
+                  <View style={styles.performanceIconContainer}>
+                    <TouchableOpacity onPress={() => this.props.goodPerformance(item.empID)}>
+                      <Icon name="ios-thumbs-up" style={styles.goodPerformance}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.props.badPerformance(item.empID)}>
+                      <Icon name="ios-thumbs-down" style={styles.badPerformance}/>
+                    </TouchableOpacity>
+                  </View>
+                </Card>
+                )
+              }
+              keyExtractor={item => item.empID.toString()}
+            >
+            </FlatList>
+        )}
 }
 
+const mapStateToProps = state => {
+  return { data: state }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    goodPerformance: id => dispatch({
+    type: "GOOD_PERFORMANCE",
+    id: id
+  }),
+    badPerformance: id => dispatch({
+      type: "BAD_PERFORMANCE",
+      id: id
+    })
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(EmployeeApp);
+
 const styles = StyleSheet.create({
+  //list card view container
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row"
   },
+  // no. of employee container
+  idContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20
+  },
+  // no. of employee text
+  idText: {
+    fontSize: 18,
+    color: "#000"
+  },
+  // name and salary text container
+  nameAndSalaryContainer: {
+    flex: 4,
+    padding: 20
+  },
+  // employee name text
+  nameText: {
+    fontSize: 16,
+    color: "#000"
+  },
+  // employee salary text
+  salaryText: {
+    fontSize: 16,
+    color: "#000"
+  },
+  // like dislike icon container
+  performanceIconContainer: {
+    flex: 2,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  //like icon
+  goodPerformance: {
+    color: "green"
+  },
+  // dislike icon
+  badPerformance: {
+    color: "red"
+  }
 });
